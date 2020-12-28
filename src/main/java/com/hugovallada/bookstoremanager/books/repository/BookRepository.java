8ecca3552b9adfe7429898407e883294894f3b7c
Hugo Vallada <valladahugo@gmail.com>
2020-12-28 13:19:56 -0300

    Create repositories classes for entities

---
+++ src/main/java/com/hugovallada/bookstoremanager/books/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.hugovallada.bookstoremanager.books.repository;
+
+import com.hugovallada.bookstoremanager.books.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
---
+++ src/main/java/com/hugovallada/bookstoremanager/publishers/repository/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.hugovallada.bookstoremanager.publishers.repository;
+
+import com.hugovallada.bookstoremanager.publishers.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+}
