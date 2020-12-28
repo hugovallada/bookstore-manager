21d308c225ad6af4595aa4c8a2b38231c87e42c9
Hugo Vallada <valladahugo@gmail.com>
2020-12-28 12:56:21 -0300

    Create auditable entity

--- src/main/java/com/hugovallada/bookstoremanager/author/entity/Author.java
+++ src/main/java/com/hugovallada/bookstoremanager/author/entity/Author.java
@@ -1,6 +1,7 @@
 package com.hugovallada.bookstoremanager.author.entity;
 
 import com.hugovallada.bookstoremanager.books.entity.Book;
+import com.hugovallada.bookstoremanager.entity.Auditable;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -8,7 +9,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class Author {
+public class Author extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
