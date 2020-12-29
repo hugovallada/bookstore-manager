96374b67f7382b891aaf4d87de41735cf9363ad5
Hugo Vallada <valladahugo@gmail.com>
2020-12-29 15:37:03 -0300

    Create author creation functionality

--- src/test/java/com/hugovallada/bookstoremanager/author/service/AuthorServiceTest.java
+++ src/test/java/com/hugovallada/bookstoremanager/author/service/AuthorServiceTest.java
@@ -2,13 +2,22 @@ package com.hugovallada.bookstoremanager.author.service;
 
 import com.hugovallada.bookstoremanager.author.builder.AuthorDTOBuilder;
 import com.hugovallada.bookstoremanager.author.dto.AuthorDTO;
+import com.hugovallada.bookstoremanager.author.exception.AuthorAlreadyExistsException;
 import com.hugovallada.bookstoremanager.author.mapper.AuthorMapper;
 import com.hugovallada.bookstoremanager.author.repository.AuthorRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
@@ -28,4 +37,38 @@ public class AuthorServiceTest {
         authorDTOBuilder = AuthorDTOBuilder.builder().build();
     }
 
+    @Test
+    void whenNewAuthorIsInformedThenInShouldBeCreated() {
+        //given
+        var expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        var expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        //when
+        Mockito.when(authorRepository.save(expectedCreatedAuthor))
+                .thenReturn(expectedCreatedAuthor);
+
+        Mockito.when(authorRepository.findByName(expectedAuthorToCreateDTO.getName()))
+                .thenReturn(Optional.empty());
+
+        var createdAuthorDTO = authorService.create(expectedAuthorToCreateDTO);
+
+        //then
+        MatcherAssert.assertThat(createdAuthorDTO, Is.is(IsEqual.equalTo(expectedAuthorToCreateDTO)));
+    }
+
+    @Test
+    void whenExistingAuthorIsInformedThenAnExceptionShouldBeThrown() {
+        //given
+        var expectedAuthorToCreateDTO = authorDTOBuilder.buildAuthorDTO();
+        var expectedCreatedAuthor = authorMapper.toModel(expectedAuthorToCreateDTO);
+
+        //when
+        Mockito.when(authorRepository.findByName(expectedAuthorToCreateDTO.getName()))
+                .thenReturn(Optional.of(expectedCreatedAuthor));
+
+        //then
+        Assertions.assertThrows(AuthorAlreadyExistsException.class, () -> authorService.create(expectedAuthorToCreateDTO));
+
+
+    }
 }
--- src/test/java/com/hugovallada/bookstoremanager/controller/AuthorControllerTest.java
+++ src/test/java/com/hugovallada/bookstoremanager/controller/AuthorControllerTest.java
@@ -1,20 +1,36 @@
 package com.hugovallada.bookstoremanager.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.hugovallada.bookstoremanager.author.builder.AuthorDTOBuilder;
 import com.hugovallada.bookstoremanager.author.controller.AuthorController;
+import com.hugovallada.bookstoremanager.author.dto.AuthorDTO;
 import com.hugovallada.bookstoremanager.author.service.AuthorService;
+import com.hugovallada.bookstoremanager.utils.JsonConversionUtils;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorControllerTest {
+
+    private static final String AUTHOR_API_URL_PATH = "/api/v1/authors";
 
     @Mock
     private AuthorService authorService;
@@ -33,5 +49,34 @@ public class AuthorControllerTest {
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
                 .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenStatusCreatedShouldBeReturned() throws Exception {
+        var expectedCreatedAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+
+        Mockito.when(authorService.create(expectedCreatedAuthorDTO))
+                .thenReturn(expectedCreatedAuthorDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(AUTHOR_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonConversionUtils.asJsonString(expectedCreatedAuthorDTO)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Is.is(expectedCreatedAuthorDTO.getId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Is.is(expectedCreatedAuthorDTO.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age", Is.is(expectedCreatedAuthorDTO.getAge())));
+    }
+
+    @Test
+    void whenPOSTIsCalledWithoutRequiredFieldThenBadRequestStatusShouldBeInformed() throws Exception {
+        var expectedCreatedAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+        expectedCreatedAuthorDTO.setName(null);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post(AUTHOR_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonConversionUtils.asJsonString(expectedCreatedAuthorDTO)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
     }
 }
